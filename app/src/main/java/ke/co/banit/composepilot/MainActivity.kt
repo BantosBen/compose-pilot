1c2c5e7f38aa5fd2cddc9a6950db566ee8db1646
Angatia Benson <angatiabenson1@gmail.con>
2023-08-09 23:47:41 -0500

    feat: completed simple Compose UI

--- app/src/main/java/ke/co/banit/composepilot/MainActivity.kt
+++ app/src/main/java/ke/co/banit/composepilot/MainActivity.kt
@@ -1,15 +1,35 @@
 package ke.co.banit.composepilot
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ke.co.banit.composepilot.ui.theme.ComposePilotTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,10 +47,46 @@ class MainActivity : ComponentActivity() {
 fun MyApp(){
     // A surface container using the 'background' color from the theme
     Surface(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 12.dp),
         color = MaterialTheme.colorScheme.background
     ) {
-        Greeting("Android")
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Kes. 100", style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
+            Spacer(modifier = Modifier.height(30.dp))
+            CreateCircle()
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun CreateCircle() {
+    Card(
+        modifier = Modifier
+            .padding(all = 4.dp)
+            .size(100.dp)
+            .background(Color(0xFF546E7A), CircleShape)
+            .clickable {
+                Log.d("TAP", "CreateCircle: TAP")
+            },
+        shape = CircleShape,
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
+    ) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(
+                text = "Tap",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(4.dp)
+            )
+        }
     }
 }
 
